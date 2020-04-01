00023f39f79c8113b345fddd9890ccc16689a15c
Romaric JODIN <rjodin@upmem.com>
2020-04-01 16:27:01 +0200

    counted_read is redondant with requests.nb_reads, let's remove it

--- host/src/dispatch.c
+++ host/src/dispatch.c
@@ -37,21 +37,20 @@ void dispatch_free(unsigned int nb_dpu)
     free(requests);
 }
 
-static void write_mem_DPU(index_seed_t *seed, int *counted_read, int8_t *read, int num_read, pthread_mutex_t *dispatch_mutex)
+static void write_mem_DPU(index_seed_t *seed, int8_t *read, int num_read, pthread_mutex_t *dispatch_mutex)
 {
     int8_t buf[SIZE_NEIGHBOUR_IN_BYTES];
 
     while (seed != NULL) {
         pthread_mutex_lock(&dispatch_mutex[seed->num_dpu]);
         {
-            int nb_read_written = counted_read[seed->num_dpu];
+            int nb_read_written = requests[seed->num_dpu].nb_reads;
             if ((nb_read_written + 1) > MAX_DPU_REQUEST) {
                 ERROR_EXIT(28, "\nBuffer full (DPU %d)", seed->num_dpu);
             }
 
             code_neighbour(&read[SIZE_SEED], buf);
             backends_functions.add_seed_to_requests(requests, num_read, nb_read_written, seed, buf);
-            counted_read[seed->num_dpu]++;
         }
         pthread_mutex_unlock(&dispatch_mutex[seed->num_dpu]);
 
@@ -63,7 +62,6 @@ typedef struct {
     index_seed_t **index_seed;
     int8_t *read_buffer;
     int nb_read;
-    int *counted_read;
     pthread_mutex_t *dispatch_mutex;
 } dispatch_thread_common_arg_t;
 
@@ -79,7 +77,6 @@ static void *dispatch_read_thread_fct(void *arg)
     index_seed_t **index_seed = args->common_arg->index_seed;
     int8_t *read_buffer = args->common_arg->read_buffer;
     int nb_read = args->common_arg->nb_read;
-    int *counted_read = args->common_arg->counted_read;
     pthread_mutex_t *dispatch_mutex = args->common_arg->dispatch_mutex;
 
     int nb_read_per_thread = nb_read / DISPATCHING_THREAD;
@@ -87,7 +84,7 @@ static void *dispatch_read_thread_fct(void *arg)
          (num_read < nb_read) && (num_read < (nb_read_per_thread * (thread_id + 1))); num_read++) {
         int8_t *read = &read_buffer[num_read * SIZE_READ];
         index_seed_t *seed = index_seed[code_seed(read)];
-        write_mem_DPU(seed, counted_read, read, num_read, dispatch_mutex);
+        write_mem_DPU(seed, read, num_read, dispatch_mutex);
     }
     return NULL;
 }
@@ -96,20 +93,17 @@ void dispatch_read(unsigned int pass_id)
 {
     int ret;
     int nb_dpu = get_nb_dpu();
-    int counted_read[nb_dpu];
     pthread_mutex_t dispatch_mutex[nb_dpu];
     dispatch_thread_common_arg_t common_arg = {
         .index_seed = index_get(),
         .read_buffer = get_reads_buffer(pass_id),
         .nb_read = get_reads_in_buffer(pass_id),
-        .counted_read = counted_read,
         .dispatch_mutex = dispatch_mutex,
     };
     pthread_t thread_id[DISPATCHING_THREAD];
     dispatch_thread_arg_t thread_arg[DISPATCHING_THREAD];
 
     for (int numdpu = 0; numdpu < nb_dpu; numdpu++) {
-        counted_read[numdpu] = 0;
         requests[numdpu].nb_reads = 0;
         pthread_mutex_init(&dispatch_mutex[numdpu], NULL);
     }
