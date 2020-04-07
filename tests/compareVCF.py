a23c71b7b29bb3f04d412cfaf68901a6e6f579f9
Romaric JODIN <rjodin@upmem.com>
2020-04-07 18:48:10 +0200

    improve python code of compareVCF

--- tests/compareVCF.py
+++ tests/compareVCF.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 import re
@@ -12,14 +12,14 @@ stat_limit = 100
 
 
 def init_stat():
-    stat = {}
-    for i in range(0, stat_limit):
-        stat[i] = [0, 0, 0]
-    return stat
+    return [[0, 0, 0] for _ in range(stat_limit)]
+
+
+extract_info_re = re.compile("DEPTH=(.*);COV=(.*);SCORE=(.*)")
 
 
 def extract_info(info):
-    regex = re.search('DEPTH=(.*);COV=(.*);SCORE=(.*)', info)
+    regex = re.search(extract_info_re, info)
     depth = int(regex.group(1))
     cov = int(regex.group(2))
     score = int(regex.group(3))
@@ -29,62 +29,45 @@ def extract_info(info):
     return depth, percentage, score
 
 
-def update_stat_no_check(stat, v):
-    info = v[2]
+def update_stat_no_check(stat, info):
     depth, percentage, score = extract_info(info)
 
-    if depth >= stat_limit:
-        depth = stat_limit - 1
-    stat[depth][0] += 1
-
-    if percentage >= stat_limit:
-        percentage = stat_limit - 1
-    stat[percentage][1] += 1
-
-    if score >= stat_limit:
-        score = stat_limit - 1
-    stat[score][2] += 1
+    stat[depth if depth < stat_limit else stat_limit - 1][0] += 1
+    stat[percentage if percentage < stat_limit else stat_limit - 1][1] += 1
+    stat[score if score < stat_limit else stat_limit - 1][2] += 1
 
 
-def update_stat(stat, v):
+def update_stat(stat, info):
     if stat is None:
         return
-    update_stat_no_check(stat, v)
+    update_stat_no_check(stat, info)
 
 
-def update_stat_for_pos_no_check(stat, V):
-    for v in V:
-        update_stat_no_check(stat, v)
-
-
-def update_stat_for_pos(stat, V):
+def update_stat_for_pos(stat, infos):
     if stat is None:
         return
-    update_stat_for_pos_no_check(stat, V)
-
-
-def update_stat_for_chr(stat, V):
-    if stat is None:
-        return
-    for v in V.values():
-        update_stat_for_pos_no_check(stat, v)
+    for info in infos.values():
+        update_stat_no_check(stat, info)
 
 
 def print_stat(tp_stat, fp_stat, nb_tp, nb_fp, nb_cm):
     if tp_stat is None or fp_stat is None:
         return
+
+    tp_acc_depth = 0
+    tp_acc_percentage = 0
+    tp_acc_score = nb_tp
+    fp_acc_depth = 0
+    fp_acc_percentage = 0
+    fp_acc_score = nb_fp
+
     if nb_fp == 0:
         nb_fp = -1
     if nb_tp == 0:
         nb_tp = -1
     if nb_cm == 0:
         nb_cm = -1
-    tp_acc_depth = 0
-    tp_acc_percentage = 0
-    tp_acc_score = 0
-    fp_acc_depth = 0
-    fp_acc_percentage = 0
-    fp_acc_score = 0
+
     print("1%% tp = %.2f%% cm, 1%% cm = %.2f%% fp" %
           ((nb_tp / float(nb_cm)), (nb_cm / float(nb_fp))))
     print("i\t| tp/depth\t\tfp/depth\t| tp/percentage\t\tfp/percentage\t| tp/score\t\tfp/score")
@@ -93,74 +76,59 @@ def print_stat(tp_stat, fp_stat, nb_tp, nb_fp, nb_cm):
           "+---------------------------------------"
           "+---------------------------------------"
           )
-    for i in range(0, stat_limit):
-        tp_acc_depth += tp_stat[i][0]
-        tp_acc_percentage += tp_stat[i][1]
-        tp_acc_score += tp_stat[i][2]
-        fp_acc_depth += fp_stat[i][0]
-        fp_acc_percentage += fp_stat[i][1]
-        fp_acc_score += fp_stat[i][2]
+    for i in range(stat_limit):
+        tp_depth = tp_stat[i][0]
+        tp_per = tp_stat[i][1]
+        tp_score = tp_stat[i][2]
+        fp_depth = fp_stat[i][0]
+        fp_per = fp_stat[i][1]
+        fp_score = fp_stat[i][2]
+        tp_acc_depth += tp_depth
+        tp_acc_percentage += tp_per
+        tp_acc_score -= tp_score
+        fp_acc_depth += fp_depth
+        fp_acc_percentage += fp_per
+        fp_acc_score -= fp_score
         print("%d\t| %.2f%% (%.2f%%)   \t%.2f%% (%.2f%%)\t| %.2f%% (%.2f%%)   \t%.2f%% (%.2f%%)\t| %.2f%% (%.2f%%)   \t%.2f%% (%.2f%%)" %
               (i,
-               per(tp_acc_depth, nb_tp),
-               per(tp_stat[i][0], nb_tp),
-               per(fp_acc_depth, nb_fp),
-               per(fp_stat[i][0], nb_fp),
-               per(tp_acc_percentage, nb_tp),
-               per(tp_stat[i][1], nb_tp),
-               per(fp_acc_percentage, nb_fp),
-               per(fp_stat[i][1], nb_fp),
-               100.0 - per(tp_acc_score, nb_tp),
-               per(tp_stat[i][2], nb_tp),
-               100.0 - per(fp_acc_score, nb_fp),
-               per(fp_stat[i][2], nb_fp),
+               per(tp_acc_depth, nb_tp), per(tp_depth, nb_tp),
+               per(fp_acc_depth, nb_fp), per(fp_depth, nb_fp),
+               per(tp_acc_percentage, nb_tp), per(tp_per, nb_tp),
+               per(fp_acc_percentage, nb_fp), per(fp_per, nb_fp),
+               per(tp_acc_score, nb_tp), per(tp_score, nb_tp),
+               per(fp_acc_score, nb_fp), per(fp_score, nb_fp),
                )
               )
 
 
-def var_match(v1, v2):
-    return v1[0] == v2[0] and v1[1] == v2[1]
-
-
-def compute_for_pos(V1, V2, tp, fp, tp_stat, fp_stat):
-    for v1 in V1:
-        found = False
-        for v2 in V2:
-            if var_match(v1, v2):
-                found = True
-                tp += 1
-                update_stat(tp_stat, v1)
-                break
-        if not found:
+def compute_for_pos(v1_infos, v2_infos, tp, fp, tp_stat, fp_stat):
+    for (ref, alt), v1_info in v1_infos.items():
+        if (ref, alt) in v2_infos:
+            tp += 1
+            update_stat(tp_stat, v1_info)
+        else:
             fp += 1
-            update_stat(fp_stat, v1)
+            update_stat(fp_stat, v1_info)
     return tp, fp
 
 
 def compute(V1, V2, tp_stat, fp_stat):
     tp = 0
     fp = 0
-    for chr, V1_chr in V1.items():
-        if chr not in V2:
-            update_stat_for_chr(fp_stat, V1_chr)
-            continue
-        for pos, V1_pos in V1_chr.items():
-            if pos in V2[chr]:
-                tp, fp = compute_for_pos(
-                    V1_pos, V2[chr][pos], tp, fp, tp_stat, fp_stat)
-            else:
-                fp += len(V1_pos)
-                update_stat_for_pos(fp_stat, V1_pos)
+    for (chr, pos), v1_infos in V1.items():
+        if (chr, pos) in V2:
+            tp, fp = compute_for_pos(
+                v1_infos, V2[(chr, pos)], tp, fp, tp_stat, fp_stat)
+        else:
+            fp += len(v1_infos)
+            update_stat_for_pos(fp_stat, v1_infos)
     return tp, fp
 
 
 def compute_data(V_ref, V_upvc, len_ref, len_upvc):
     enable_stat = False
-    tp_stat = None
-    fp_stat = None
-    if enable_stat:
-        tp_stat = init_stat()
-        fp_stat = init_stat()
+    tp_stat = init_stat() if enable_stat else None
+    fp_stat = init_stat() if enable_stat else None
 
     tp, fp = compute(V_upvc, V_ref, tp_stat, fp_stat)
     cm, fn = compute(V_ref, V_upvc, None, None)
@@ -182,9 +150,7 @@ def compute_data(V_ref, V_upvc, len_ref, len_upvc):
 
 def filter(info, per_filter, score_filter):
     depth, percentage, score = extract_info(info)
-    if score <= score_filter and percentage >= per_filter:
-        return True
-    return False
+    return score <= score_filter and percentage >= per_filter
 
 
 def get_data(filename, filter_enable, per_filter, score_filter):
@@ -192,55 +158,41 @@ def get_data(filename, filter_enable, per_filter, score_filter):
     SUB = {}
     INS = {}
     DEL = {}
-    ff = open(filename)
-    line = ff.readline()
     len_sub = 0
     len_ins = 0
     len_del = 0
-    while line != "":
-        if line[0] == "#":
-            line = ff.readline()
+
+    for line in open(filename, "r").readlines():
+        if len(line) < 1 or line[0] == "#":
             continue
-        L = line.split("\t")
-        if L[0] == "X":
+
+        chr_str, pos_str, _, ref, alt, _, _, info = line.split("\t")
+
+        if chr_str == "X":
             chr = 23
-        elif L[0] == "Y":
+        elif chr_str == "Y":
             chr = 24
         else:
-            chr = int(re.search('[^0-9]*(.*)', L[0]).group(1))
-        pos = int(L[1])
-        ref = L[3]
-        alt = L[4]
-        info = L[7]
-        if len(ref) > 1 and len(alt) <= 1 and (not filter_enable or filter(info, per_filter, score_filter)):
-            if chr not in DEL:
-                DEL[chr] = {}
-            if pos not in DEL[chr]:
-                DEL[chr][pos] = [[ref, alt, info]]
-            else:
-                DEL[chr][pos].append([ref, alt, info])
-            len_del += 1
-        elif len(alt) > 1 and len(ref) <= 1 and (not filter_enable or filter(info, per_filter, score_filter)):
-            if chr not in INS:
-                INS[chr] = {}
-            if pos not in INS[chr]:
-                INS[chr][pos] = [[ref, alt, info]]
-            else:
-                INS[chr][pos].append([ref, alt, info])
-            len_ins += 1
-        elif len(ref) == 1 and len(alt) == 1 and (not filter_enable or filter(info, per_filter, score_filter)):
-            if chr not in SUB:
-                SUB[chr] = {}
-            if pos not in SUB[chr]:
-                SUB[chr][pos] = [[ref, alt, info]]
-            else:
-                SUB[chr][pos].append([ref, alt, info])
-            len_sub += 1
-        line = ff.readline()
+            chr = int(chr_str.strip("chr"))
 
-    ff.close()
+            pos = int(pos_str)
+
+            if len(ref) > 1 and len(alt) <= 1 \
+               and (not filter_enable or filter(info, per_filter, score_filter)):
+                DEL.setdefault((chr, pos), {})[(ref, alt)] = info
+                len_del += 1
+            elif len(alt) > 1 and len(ref) <= 1 \
+                 and (not filter_enable or filter(info, per_filter, score_filter)):
+                INS.setdefault((chr, pos), {})[(ref, alt)] = info
+                len_ins += 1
+            elif len(ref) == 1 and len(alt) == 1 \
+                 and (not filter_enable or filter(info, per_filter, score_filter)):
+                SUB.setdefault((chr, pos), {})[(ref, alt)] = info
+                len_sub += 1
+
     print(len_sub, len_ins, len_del)
     return SUB, INS, DEL, len_sub, len_ins, len_del
+
 
 ref_file = sys.argv[1]
 upvc_file = sys.argv[2]
