80d2e60a0de242bddf2083ca8a40e9e13cf625ef
Jordi Chauzi <jchauzi@upmem.com>
2020-11-30 16:29:08 +0100

    Make __M aligned on 8 bytes
    This seems to be necessary to run correctly.

--- dpu/src/odpd_init_opt.c
+++ dpu/src/odpd_init_opt.c
@@ -2,8 +2,10 @@
  * Copyright 2016-2019 - Dominique Lavenier & UPMEM
  */
 
+#include <attributes.h>
+
 #include "common.h"
 #include "odpd.h"
 
 /* Need big space to store one triplet of matrices per tasklet */
-uint8_t __M[3 * NR_TASKLETS * SIZEOF_MATRIX(NB_BYTES_TO_SYMS(SIZE_NEIGHBOUR_IN_BYTES, 0))];
+__dma_aligned uint8_t __M[3 * NR_TASKLETS * SIZEOF_MATRIX(NB_BYTES_TO_SYMS(SIZE_NEIGHBOUR_IN_BYTES, 0))];
