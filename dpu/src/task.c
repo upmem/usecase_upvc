5cef587b13458845e030c2d36eb32f878cef56a7
amoisson <amoisson@upmem.com>
2022-05-18 14:58:38 +0200

    reduced max_score back to 40. It seems to have improved both algorithms Oo

--- dpu/src/task.c
+++ dpu/src/task.c
@@ -56,9 +56,9 @@ __host dpu_compute_time_t DPU_COMPUTE_TIME_VAR;
  * @brief Maximum score allowed.
  */
 #if SIZE_READ>120
-#define MAX_SCORE 80
+#define MAX_SCORE 40
 #else
-#define MAX_SCORE 80
+#define MAX_SCORE 40
 #endif
 
 /**
